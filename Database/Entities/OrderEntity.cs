ade3ee56e4b8f91776bf3c5535c62ab4f06de5d6
Adam Kusiakiewicz <adam.kusiakiewicz@gmail.com>
2024-01-23 21:06:44 +0100

    Last changes

--- Database/Entities/OrderEntity.cs
+++ Database/Entities/OrderEntity.cs
@@ -1,12 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace OrderTracker.Database.Entities
 {
+    public enum OrderStatus
+    {
+        Opłacone,
+        Wysłane,
+        Dostarczone,
+        Odebrane,
+    }
     public class OrderEntity
     {   
         public int Id {get;set;}
         public string CustomerName {get;set;}
         public double Price {get;set;}
         public DateTime Date {get;set;} = DateTime.Now;
-        public string Status {get;set;}
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public OrderStatus Status {get;set;}
         public DateTime? ModifiedStatus {get;set;}
     }
 }
